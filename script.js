4244b141046d2fb5e3bed09279ac5d14e412764a
Feng Tian <fengtian@Fengs-MacBook-Pro.local>
2024-01-04 21:46:44 +1100

    Add Javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // This function runs when the DOM is fully loaded
+
+    // Example: Change the color of the heading in the "Home" section
+    changeHeadingColor();
+});
+
+function changeHeadingColor() {
+    // Get the heading element in the "Home" section
+    var homeHeading = document.querySelector('#home h1');
+
+    // Change the color of the heading
+    homeHeading.style.color = getRandomColor();
+}
+
+function getRandomColor() {
+    // Generate a random color in hexadecimal format
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
